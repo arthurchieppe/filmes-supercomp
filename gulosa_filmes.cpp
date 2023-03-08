2598a2fc31c508d6d8087fd27ab1206f09ab0d4c
arthurchieppe <agchieppe@gmail.com>
2023-03-08 15:23:39 -0300

    Filmes gulosa feito e validado com Fran, falta output grafico

--- gulosa_filmes.cpp
+++ gulosa_filmes.cpp
@@ -1,20 +1,35 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-struct filme
-    {
-        int h_inicio;
-        int h_fim;
-        int cat;
-    };
+struct filme {
+    int h_inicio;
+    int h_fim;
+    int cat;
+};
+
+bool is_available(filme &el, bool timetable[24]) {
+    for (int i = el.h_inicio; i < el.h_fim; i++) {
+        if (timetable[i] == true)
+            return false;
+    }
+    return true;
+}
+
+void print_programacao(vector<filme> &programacao) {
+    for (auto &el : programacao) {
+        cout << el.h_inicio << " " << el.h_fim << " " << el.cat << "\n";
+    }
+}
 
 int main(int argc, char **argv) {
     vector<filme> filmes;
     vector<filme> programacao;
     vector<int> categorias;
+
+    bool timetable[24] = {false};
 
     int n_filmes = 0;
     int n_cat = 0;
@@ -34,12 +49,22 @@ int main(int argc, char **argv) {
         filmes.push_back({h_inicio, h_fim, cat});
     }
 
-    sort(filmes.begin(), filmes.end(), [](auto &i, auto &j)
-         { return i.h_fim < j.h_fim; });
+    sort(filmes.begin(), filmes.end(), [](auto &i, auto &j) { return i.h_fim < j.h_fim; });
 
     for (auto &el : filmes) {
-        cout << el.h_inicio << " " << el.h_fim << " " << el.cat << "\n";
-    }
+        if (el.h_inicio >= el.h_fim)
+            continue;
+        if (categorias[el.cat - 1] == 0)
+            continue;
 
+        if (is_available(el, timetable)) {
+            for (int i = el.h_inicio; i < el.h_fim; i++) {
+                timetable[i] = true;
+            }
+            categorias[el.cat - 1]--;
+            programacao.push_back(el);
+        }
+    }
+    print_programacao(programacao);
     return 0;
 }
