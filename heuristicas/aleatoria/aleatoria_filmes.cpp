bad3f956afcf038e7ae88c741360b1a2a516a267
arthurchieppe <agchieppe@gmail.com>
2023-03-28 14:34:28 +0000

    Aleatoria feita, falta output grafico e output numero de filmes cpp

---
+++ heuristicas/aleatoria/aleatoria_filmes.cpp
@@ -0,0 +1,120 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+struct filme {
+    int h_inicio;
+    int h_fim;
+    int cat;
+};
+
+bool is_available(filme &el, bool (&timetable)[24], vector<int> &categorias) {
+    if (el.h_inicio > el.h_fim)
+        return false;
+    if (categorias[el.cat - 1] == 0)
+        return false;
+    for (int i = el.h_inicio; i < el.h_fim; i++) {
+        if (timetable[i] == true)
+            return false;
+    }
+    return true;
+}
+
+void print_filmes(vector<filme> &filmes) {
+    for (auto &el : filmes) {
+        cout << el.h_inicio << " " << el.h_fim << " " << el.cat << "\n";
+    }
+}
+
+bool should_break(int &total_number_of_hours, long unsigned int &allowed_number_movies, vector<filme> &programacao) {
+    if (total_number_of_hours == 24)
+        return true;
+    if (allowed_number_movies == (programacao.size()))
+        return true;
+    return false;
+}
+
+int main(int argc, char **argv) {
+    vector<filme> filmes;
+    vector<filme> programacao;
+    vector<int> categorias;
+
+    bool timetable[24] = {false};
+
+    int n_filmes = 0;
+    int n_cat = 0;
+    int temp = 0;
+    long unsigned int allowed_number_movies = 0;
+
+    cin >> n_filmes >> n_cat;
+    filmes.reserve(n_filmes);
+
+    // Gerador random:
+    default_random_engine generator;
+    generator.seed(42);
+
+    uniform_real_distribution<double>
+        distribution(0.0, 1.0);
+
+    for (int i = 0; i < n_cat; i++) {
+        cin >> temp;
+        categorias.push_back(temp);
+        allowed_number_movies += temp;
+    }
+
+    int h_inicio, h_fim, cat;
+    for (int i = 0; i < n_filmes; i++) {
+        cin >> h_inicio >> h_fim >> cat;
+        filmes.push_back({h_inicio, h_fim, cat});
+    }
+
+    sort(filmes.begin(), filmes.end(), [](auto &i, auto &j) { return i.h_fim < j.h_fim; });
+    // Print all filmes
+
+    // print_filmes(filmes);
+
+    int total_number_of_hours = 0;
+    int i = 1; // Posicao do item nao obtido
+
+    for (auto &el : filmes) {
+        if (should_break(total_number_of_hours, allowed_number_movies, programacao))
+            break;
+
+        // Nao aceita filmes que comecam, por ex, 23 e terminam 1
+        if (is_available(el, timetable, categorias)) {
+            for (int i = el.h_inicio; i < el.h_fim; i++) {
+                timetable[i] = true;
+                total_number_of_hours++;
+            }
+            categorias[el.cat - 1]--;
+            programacao.push_back(el);
+        }
+
+        if (should_break(total_number_of_hours, allowed_number_movies, programacao))
+            break;
+
+        if (distribution(generator) > 0.75 && i < n_filmes) {
+            uniform_int_distribution<int> distributionInt(i, n_filmes - 1);
+            int p = distributionInt(generator);
+
+            if (is_available(filmes[p], timetable, categorias)) {
+                for (int i = filmes[p].h_inicio; i < filmes[p].h_fim; i++) {
+                    timetable[i] = true;
+                    total_number_of_hours++;
+                }
+                categorias[el.cat - 1]--;
+                programacao.push_back(filmes[p]);
+
+                filmes.erase(filmes.begin() + p - 1);
+                n_filmes -= 1;
+            }
+        }
+        i = i + 1;
+    }
+    // print_filmes(programacao);
+    return 0;
+}
