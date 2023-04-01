5c38d4cf9c0af35363536a4bb1895cbafb5c86f8
arthurchieppe <agchieppe@gmail.com>
2023-03-31 23:25:48 -0300

    Codigo fonte comentado

--- heuristicas/gulosa/gulosa_filmes.cpp
+++ heuristicas/gulosa/gulosa_filmes.cpp
@@ -11,6 +11,7 @@ struct filme {
     int cat;
 };
 
+// Check if a movie is available to be scheduled given the timetable and the availability of its category
 bool is_available(filme &el, bool (&timetable)[24], vector<int> &categorias) {
     if (el.h_inicio >= el.h_fim)
         return false;
@@ -23,6 +24,7 @@ bool is_available(filme &el, bool (&timetable)[24], vector<int> &categorias) {
     return true;
 }
 
+// Print the list of scheduled movies along with the total number of movies and hours scheduled
 void print_filmes(vector<filme> &filmes, int &total_number_of_hours) {
     cout << "Numero total de filmes: " << filmes.size() << "\n\n";
     cout << "Numero total de horas: " << total_number_of_hours << "\n\n";
@@ -37,6 +39,7 @@ void print_filmes(vector<filme> &filmes, int &total_number_of_hours) {
              << left << setw(10) << el.cat << '\n';
     }
 }
+// Check if the scheduling should be terminated given the total number of hours scheduled and the number of movies allowed
 
 bool should_break(int &total_number_of_hours, long unsigned int &allowed_number_movies, vector<filme> &programacao) {
     if (total_number_of_hours == 23) // 23 é o número máximo de filmes que podem ser exibidos
@@ -46,6 +49,7 @@ bool should_break(int &total_number_of_hours, long unsigned int &allowed_number_
     return false;
 }
 
+// Add a movie to the timetable, decrease the availability of its category, and add it to the list of scheduled movies
 void add_to_timetable(filme &el, bool (&timetable)[24], vector<int> &categorias, vector<filme> &programacao, int &total_number_of_hours) {
     for (int i = el.h_inicio; i < el.h_fim; i++) {
         timetable[i] = true;
@@ -60,12 +64,12 @@ int main(int argc, char **argv) {
     vector<filme> programacao;
     vector<int> categorias;
 
-    bool timetable[24] = {false};
+    bool timetable[24] = {false}; // Quadro de horarios que representa as 24 horas do dia
 
     int n_filmes = 0;
     int n_cat = 0;
     int temp = 0;
-    long unsigned int allowed_number_movies = 0;
+    long unsigned int allowed_number_movies = 0; // Soma dos filmes permitidos por cada categoria
 
     cin >> n_filmes >> n_cat;
     filmes.reserve(n_filmes);
@@ -83,9 +87,6 @@ int main(int argc, char **argv) {
     }
 
     sort(filmes.begin(), filmes.end(), [](auto &i, auto &j) { return i.h_fim < j.h_fim; });
-    // Print all filmes
-
-    // print_filmes(filmes);
 
     int total_number_of_hours = 0;
 
